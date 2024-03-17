318a38696f752c4f081cfc6edcf1c87f841b1e00
Navaphan Singkaew <140819697+copcrush@users.noreply.github.com>
2024-03-17 21:09:39 +0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
+import ProjectsSection from "./components/ProjectsSection";
+import ContactSection from "./components/ContactSection";
+import FooterSection from "./components/FooterSection";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection id="about" />
+        <ProjectsSection id="project" />
+        <ContactSection id="contact" />
+        <FooterSection />
+      </div>
+    </main>
+  );
+}
+
